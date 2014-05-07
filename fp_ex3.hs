800395b2c0044f342158af8aad4e7e72b6f0626f
Tom Janson <priv.tom.janson@gmail.com>
2014-05-07 15:58:10 +0200

    cantor

--- fp_ex3.hs
+++ fp_ex3.hs
@@ -5,6 +5,7 @@
 
 -- Exercise sheet 3
 import Data.List(inits)
+import Data.Ratio
 
 -- E1
 -- a)
@@ -13,21 +14,39 @@ type Group = Int
 
 count :: Int -> [Pupil] -> [(Group, Pupil)]
 count n = zip xs where xs = 1:map ((1+) . (`mod` n)) xs
+
+count' n = zip (cycle [1..n]) -- way nicer!
+
 -- Test: count 3 ["Pete", "Frank", "Sandra", "Caro", "Max"]
 -- >> [(1,"Pete"),(2,"Frank"),(3,"Sandra"),(1,"Caro"),(2,"Max")]
 
 -- b) TODO
--- cantor :: Int -> Rational
+cantor :: Int -> Rational
+cantor = (!!) cantorAll
+
+cantorAll :: [Rational]
+cantorAll = foldr (++) [] $ map cantorH [1..]
+
+tupleToFrac :: (Integer,Integer) -> Rational
+tupleToFrac (a,b) = a % b
+
+cantorH :: Integer -> [Rational] --Rational
+cantorH x = let l = [1..x] in
+                map tupleToFrac (filter isUniqe $ zip l (reverse l))
+                where isUniqe (a,b) = gcd a b == 1
 
 -- E2
 -- a) TODO
 
--- prefixsum :: [Int] -> [Int]
 -- list comprehension version
+prefixsum :: [Int] -> [Int]
+prefixsum l = [ sum x | x <- tail $ inits l ] 
+--prefixsum l = [ sum x | x <- inits l, not $ null x ] 
 
 prefixsum' :: [Int] -> [Int]
 prefixsum' = map sum . tail . inits
--- Test: prefixsum' [2,4,5,0,1]
+
+-- Test: prefixsum [2,4,5,0,1]
 -- >> [2,6,11,11,12]
 
 -- b) TODO
