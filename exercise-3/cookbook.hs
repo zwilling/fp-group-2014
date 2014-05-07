fcd7b8b7dff00556f1a79c999e22d65d5a822b61
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2014-05-07 20:43:02 +0200

    Ex 3/3/a: implemented

--- exercise-3/cookbook.hs
+++ exercise-3/cookbook.hs
@@ -8,11 +8,11 @@ main = do
   if (null recipes) then putStrLn "No recipes available..."
   else do
     promptForInput recipes
-    meal <- readInput recipes
-    if (isExitCommand meal) then putStrLn "Leaving, good bye..."
-    else do
-      showRecipe meal
-      main
+--     meal <- readInput recipes
+--     if (isExitCommand meal) then putStrLn "Leaving, good bye..."
+--     else do
+--       showRecipe meal
+--       main
 
 findRecipes :: IO [String]
 findRecipes = do
@@ -28,24 +28,27 @@ removeEnding str = takeWhile (/='.') str
 -- list all available meals and ask the user to input the name of the
 -- desired meal
 promptForInput :: [String] -> IO ()
-promptForInput recipes = -- TODO
+promptForInput recipes = putStr "available recipes: " >> putStrList recipes >> putStr "\nWhat do you want to cook?\n"
+                         where putStrList [x] = putStr x
+                               putStrList (x:xs) = putStr x >> putStr ", " >> putStrList xs
+
 
 -- reads the input from the user until he either inputs the name of an
 -- existing meal or an exit command (see isExitCommand)
-readInput :: [String] -> IO String
-readInput recipes = do
-  -- TODO read the name of the meal from the command line
-  -- the result should be available in the variable 'meal'
-  if (not (meal `elem` recipes) && not (isExitCommand meal))
-  then do
-    putStrLn "I do not know a recipe for this meal, try again..."
-    promptForInput recipes
-    readInput recipes
-  else -- TODO
+-- readInput :: [String] -> IO String
+-- readInput recipes = do
+--   -- TODO read the name of the meal from the command line
+--   -- the result should be available in the variable 'meal'
+--   if (not (meal `elem` recipes) && not (isExitCommand meal))
+--   then do
+--     putStrLn "I do not know a recipe for this meal, try again..."
+--     promptForInput recipes
+--     readInput recipes
+--   else -- TODO
 
 isExitCommand :: String -> Bool
 isExitCommand cmd = (map toLower cmd) `elem` ["quit", "exit", "q", "e"]
 
 -- print the recipe with the given name on the command line
-showRecipe :: String -> IO ()
-showRecipe meal = -- TODO
+-- showRecipe :: String -> IO ()
+-- showRecipe meal = -- TODO
